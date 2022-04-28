b184945b5588136cd5bb55a75d5645133a352425
betaluis <luis.betancourt.dev@gmail.com>
2022-04-28 11:05:51 -0500

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,41 @@
-export default function Recipes() {
+import { createClient } from 'contentful';
+import Recipe from '../components/RecipeCard';
+
+export async function getStaticProps() {
+
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries({ content_type: 'recipe' });
+  
+  return {
+    props: {
+      recipes: res.items
+    }
+  }
+
+}
+
+export default function Recipes({ recipes }) {
+  console.log(recipes);
   return (
     <div className="recipe-list">
-      Recipe List
+      {
+        recipes.map(recipe => (
+          <Recipe key={recipe.sys.id} recipe={recipe} />
+        ))
+      }
+      <style jsx>
+      {`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          gap: 1rem;
+        }
+      `}
+      </style>
     </div>
   )
 }
